4a6d3e11390015f7ac4b4a7bf6ae30caabbddc82
Francesca Oliverio <francesca.oliverio@edu-its.it>
2026-06-30 21:16:04 +0200

    feat(components): create MealCard that links to recipe details

---
+++ src/components/MealCard.tsx
@@ -0,0 +1,23 @@
+// src\components\MealCard.tsx
+
+import { Text, Image, TouchableOpacity, View } from "react-native"
+import { MealSummary } from "../types/meal"
+import FavoriteBtn from "./FavoriteBtn"
+import { styles } from "../theme/styles"
+
+interface Props {
+  meal: MealSummary
+  onPress: () => void
+}
+
+export default function MealCard({ meal, onPress }: Props) {
+  return (
+    <TouchableOpacity onPress={onPress} style={[styles.containerDashed, { flexDirection: "row", alignItems: "center" }]}>
+      <Image source={{ uri: meal.strMealThumb }} style={{ width: 50, height: 50, borderRadius: 12 }} />
+      <View style={{ flex: 1 }}>
+        <Text style={styles.strongText}>{meal.strMeal}</Text>
+      </View>
+      <FavoriteBtn idMeal={meal.idMeal} />
+    </TouchableOpacity>
+  )
+}
