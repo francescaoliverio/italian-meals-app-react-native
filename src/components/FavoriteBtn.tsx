76fdedccd579e4fcadd57e59f99ee5811aa8c13a
Francesca Oliverio <francesca.oliverio@edu-its.it>
2026-06-30 21:15:10 +0200

    feat(components): create heart button for favorites

---
+++ src/components/FavoriteBtn.tsx
@@ -0,0 +1,21 @@
+// src\components\FavoriteBtn.tsx
+
+import { Pressable, Text } from "react-native"
+import { useFavorites } from "../context/FavoritesContext"
+
+interface Props {
+  idMeal: string
+}
+
+export default function FavoriteBtn({ idMeal }: Props) {
+  const { favoriteIds, toggleFavorite } = useFavorites()
+  const active = favoriteIds.includes(idMeal)
+
+  return (
+    <Pressable onPress={() => toggleFavorite(idMeal)}>
+      <Text style={{ fontSize: 24, color: "#f24" }} accessibilityRole="button" accessibilityLabel="Add to favorites">
+        {active ? "♥" : "♡"}
+      </Text>
+    </Pressable>
+  )
+}
